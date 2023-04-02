82d00c2ebe5207e2d22f1c34034b150dbd1e0f26
OkamiiYuki <129645908+OkamiiYuki@users.noreply.github.com>
2023-04-02 07:55:32 +0200

    Update Items.java

--- src/main/java/com/mpbb/ironbows/item/Items.java
+++ src/main/java/com/mpbb/ironbows/item/Items.java
@@ -14,6 +14,4 @@ public class Items {
 	public static final RegistryObject<Item> GOLDEN_BOW = BOWS.register("golden_bow", () -> new TieredBowItem(BowTiers.GOLDEN, new Item.Properties()));
 	public static final RegistryObject<Item> DIAMOND_BOW = BOWS.register("diamond_bow", () -> new TieredBowItem(BowTiers.DIAMOND, new Item.Properties()));
 	public static final RegistryObject<Item> EMERALD_BOW = BOWS.register("emerald_bow", () -> new TieredBowItem(BowTiers.EMERALD, new Item.Properties()));
-	public static final RegistryObject<Item> NETHERITE_BOW = BOWS.register("netherite_bow", () -> new TieredBowItem(BowTiers.NETHERITE, new Item.Properties()));
-}
 
